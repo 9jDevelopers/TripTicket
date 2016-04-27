79a71838ad39b588657565337ccc26eebcaea1a1
LLyouenjoy <LLyouenjoy@LLYOUENJOY-PC>
2016-04-27 08:26:54 +0800

    1

--- Travel/Entitytip/Tickets.cs
+++ Travel/Entitytip/Tickets.cs
@@ -13,29 +13,57 @@ namespace Entitytip
 {
     public class Tickets
     {
-        public Tickets Get(string tktname)
+
+        public string Get(int Eid)
         {
+
             DbHelper db = new DbHelper();
-            DbCommand cmd = db.GetSqlStringCommond("select ticketName from Tickets where ticketID=1");
-            //db.AddInParameter(cmd, "", DbType.String, tktname);
-
+            DbCommand cmd = db.GetSqlStringCommond("select ticketName,ticketPrice from TicketType where TicketTypeID=@ticketID");
+            db.AddInParameter(cmd, "@ticketID", DbType.String, Eid);
             DataTable dt = db.ExecuteDataTable(cmd);
-
-
             if (dt.Rows.Count > 0)
             {
-                // return new Tickets(tktname);
-                return null;
-
+                return dt.Rows[0][0].ToString() + "$" + dt.Rows[0][1].ToString();
             }
-            //登陆成功
-
             else
             {
                 return null;
             }
-        }       
+        }
+        public string SET(string ticketname, string ticketprice, string date, string ticketholder, int idcardno, int telenum, int safe)
+        {
+            string safetype;
+            if (safe > 0 && safe < 6)
+            {
+                safetype = "1";
+            }
+            else
+            {
+                safetype = "2";
+            }
+            DbHelper db = new DbHelper();
+            DbCommand cmd = db.GetSqlStringCommond("insert into Tickets values(2,@TicketNmae,@TicketPrice,@TicketUser ,@TicketUserIDcard,@Phone ,@TicketDate ,@TicketSafe)");
+            db.AddInParameter(cmd, "@TicketNmae", DbType.String, ticketname);
+            db.AddInParameter(cmd, "@TicketPrice", DbType.String, ticketprice);
+            db.AddInParameter(cmd, "@TicketUser", DbType.String, ticketholder);
+            db.AddInParameter(cmd, "@TicketUserIDcard", DbType.String, idcardno);
+            db.AddInParameter(cmd, "@Phone", DbType.String, telenum);
+            db.AddInParameter(cmd, "@TicketDate", DbType.String, date);
+            db.AddInParameter(cmd, "@TicketSafe", DbType.String, safetype);
+            cmd.Connection.Open();
+            int result = cmd.ExecuteNonQuery();
+            //DataTable dt = db.ExecuteDataTable(cmd);
+            if (result > 0)
+            {
+                Console.WriteLine("成功");
+                return null;
+            }
+            else
+            {
+                return null;
+            }
 
+        }
         //票id
         public int ticketID;
         //票名称
@@ -51,7 +79,7 @@ namespace Entitytip
         //是否购保险
         public bool ticketSafe;
         //票状态
-        //public TicketStatus ticketstatus;
+        public TicketStatus ticketstatus;
 
     }
 }
---
+++ Travel/Travel/ByTkt.ashx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BL;
+
+namespace Travel
+{
+    /// <summary>
+    /// ByTkt 的摘要说明
+    /// </summary>
+    public class ByTkt : IHttpHandler
+    {
+        public int tktid = 1;
+
+        public double tktprice;
+        public void ProcessRequest(HttpContext context)
+        {
+            string action = context.Request["action"];
+            switch (action)
+            {
+                case "GetPrice":
+                    BBuyTicket bbt = new BBuyTicket();
+                    string np = bbt.Gettktname(tktid);
+                    string[] npArray = np.Split('$');
+                    tktprice = double.Parse(npArray[1]);
+                    context.Response.Write(tktprice);
+                    break;
+                case "Set":
+                    string tname = context.Request.Form["jsticketname"];
+                    string pf = context.Request.Form["jsticketprice"];
+                    string dt = context.Request.Form["jsdate"];
+                    string th = context.Request.Form["jsticketholder"];
+                    int icn = int.Parse(context.Request.Form["jsidcardno"]);
+                    int tn = int.Parse(context.Request.Form["jstelenum"]);
+                    int sf = int.Parse(context.Request.Form["jssafe"]);
+                    BBuyTicket bbt1 = new BBuyTicket();
+                    bbt1.Set(tname, pf, dt, th, icn, tn, sf);
+                    break;
+
+                default: break;
+            }
+
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
