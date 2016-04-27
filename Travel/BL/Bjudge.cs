adf076d70169ec409b81eb3c80c5dc16968a5e20
炫炩月 <xuanlingyue@outlook.com>
2016-04-27 11:40:56 +0800

    t

--- Travel/BL/Bjudge.cs
+++ Travel/BL/Bjudge.cs
@@ -3,18 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Entitytip;
 
 namespace BL
 {
     public class Bjudge
     {
-        public string get(string titlename)
+        public string get(int vid)
         {
-            if(titlename!="")
+            if(vid!=0)
             {
-
-                return ;
+                judgeing jd = new Entitytip.judgeing();
+                
+                return jd.GET(vid);
             }
             else return null;
 
