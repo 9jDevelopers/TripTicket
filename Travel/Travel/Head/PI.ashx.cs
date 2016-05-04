7ebbca907715b4f6a07fa38065c436c14b0ffbbd
韩宇鹏 <973622442@qq.com>
2016-05-04 15:52:40 +0800

    完结

--- Travel/Travel/Head/PI.ashx.cs
+++ Travel/Travel/Head/PI.ashx.cs
@@ -25,44 +25,52 @@ namespace Travel.Head
                 string sex = context.Request.Form["sex"];
                 string birthday = context.Request.Form["birthday"];
                 string home = context.Request.Form["home"];
+                analyze(ref email);
+                analyze(ref username);
+                analyze(ref name);
+                analyze(ref sex);
+                analyze(ref home);
                 EditPI ep = new EditPI();
                 if (ep.Beditpi(phone, email, username, name, sex, birthday, home))
                 {
-                    context.Response.Write("修改成功");
+                    context.Response.Write(1);
                 }
                 else
                 {
-                    context.Response.Write("修改失败");
+                    context.Response.Write(0);
                 }
             }
+            if(action=="getinfo")
+            {
+                string photo;//后期完善
+                int phone;
+                string email;
+                string username;
+                string name;
+                string sex;
+                string birthday;
+                string home;
 
-            //string photo;//后期完善
-            //int phone;
-            //string email;
-            //string username;
-            //string name;
-            //string sex;
-            //string birthday;
-            //string home;
-
-            ////phone = "1234567890";
-            //string p = context.Request.Form["phone"];
-            //phone =Convert.ToInt32(p);
-            //BGetInfo getif = new BGetInfo();
-            //getif.BGI(phone, out email, out username, out name, out sex, out birthday, out home);
-            //analyze(ref email);
-            //analyze(ref username);
-            //analyze(ref sex);
-            //analyze(ref name);
-            //analyze(ref birthday);
-            //analyze(ref home);
-            //string _email="{\"email\":\""+email+"\",";
-            //string _username = "\"username\":\"" + username + "\",";
-            //string _name = "\"name\":\"" + name + "\","; ;
-            //string _sex = "\"sex\":\"" + sex + "\","; ;
-            //string _birthday = "\"birthday\":\"" + birthday + "\","; ;
-            //string _home = "\"home\":\"" + home + "\"}"; ;
-            //context.Response.Write(_email+_username+_name+_sex+_birthday+_home);
+                //phone = "1234567890";
+                string p = context.Request.Form["phone"];
+                phone = Convert.ToInt32(p);
+                BGetInfo getif = new BGetInfo();
+                getif.BGI(phone, out email, out username, out name, out sex, out birthday, out home);
+                analyze(ref email);
+                analyze(ref username);
+                analyze(ref sex);
+                analyze(ref name);
+                analyze(ref birthday);
+                analyze(ref home);
+                string _phone = "{\"phone\":\"" + p + "\",";
+                string _email = "\"email\":\"" + email + "\",";
+                string _username = "\"username\":\"" + username + "\",";
+                string _name = "\"name\":\"" + name + "\","; ;
+                string _sex = "\"sex\":\"" + sex + "\","; ;
+                string _birthday = "\"birthday\":\"" + birthday + "\","; ;
+                string _home = "\"home\":\"" + home + "\"}"; ;
+                context.Response.Write(_phone+_email + _username + _name + _sex + _birthday + _home); 
+            }
         }
 
         public void analyze(ref string text)
