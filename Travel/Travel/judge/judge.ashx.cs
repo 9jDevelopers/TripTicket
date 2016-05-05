28e43b92f05770f538a1a94060e7a5a7dfb36333
炫炩月 <xuanlingyue@outlook.com>
2016-05-05 17:36:50 +0800

    CS

--- Travel/Travel/judge/judge.ashx.cs
+++ Travel/Travel/judge/judge.ashx.cs
@@ -11,28 +11,46 @@ namespace Travel.judge
     /// </summary>
     public class judge1 : IHttpHandler
     {
+                                             
 
         public void ProcessRequest(HttpContext context)
         {
-            int  jgname= int.Parse(context.Request.Form["jgid"]);        //1.界面交互层
             Bjudge bj = new Bjudge();                                    //实体化Bjudge类。
+            int jgname = int.Parse(context.Request.Form["jgid"]);        //1.界面交互层
+
             context.Response.Write(bj.get(jgname));                      //调用BL层带参函数（get（jgname））。
 
+            if(context.Request["Evaluationofgood"] != null)
+            {
+                string Evaluationofgood = context.Request.Form["Evaluationofgood"];
+                string Evaluationservice = context.Request.Form["Evaluationservice"];
 
-            string Evaluationofgood = context.Request.Form["Evaluationofgood"];
-            string Evaluationservice = context.Request.Form["Evaluationservice"];
+
+                //string userID = context.Request.Form["userID"];
+                //string SunPhoto = context.Request.Form["SunPhoto"];
+                //string onservice = context.Request.Form["onservice"];
+                //string Chaseratings = context.Request.Form["Chaseratings"];
+                //string describe = context.Request.Form["describe"];
+                //string Sellerservice = context.Request.Form["Sellerservice"];
+                //string logisticsservice = context.Request.Form["logisticsservice"];
 
 
-            //string userID = context.Request.Form["userID"];
-            //string SunPhoto = context.Request.Form["SunPhoto"];
-            //string onservice = context.Request.Form["onservice"];
-            //string Chaseratings = context.Request.Form["Chaseratings"];
-            //string describe = context.Request.Form["describe"];
-            //string Sellerservice = context.Request.Form["Sellerservice"];
-            //string logisticsservice = context.Request.Form["logisticsservice"];
+                //context.Response.Write(Evaluationofgood +Evaluationservice);
+
+
+                if (bj.ToString(Evaluationofgood, Evaluationservice))
+                {
+                    context.Response.Write(1);
+                }
+                else
+                {
+                    context.Response.Write(0);
+                }
+
+            }
+
             
-             
-            context.Response.Write(Evaluationofgood +Evaluationservice);
+
         }
 
         public bool IsReusable
