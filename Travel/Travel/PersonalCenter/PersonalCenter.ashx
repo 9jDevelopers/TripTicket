b0fd8fb41c9723174a3912672d17551a46e64546
韩宇鹏 <973622442@qq.com>
2016-06-02 10:32:09 +0800

    123456

---
+++ Travel/Travel/PersonalCenter/PersonalCenter.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="PersonalCenter.ashx.cs" Class="Travel.PersonalCenter.PersonalCenter1" %>
---
+++ Travel/Travel/PersonalCenter/PersonalCenter.ashx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Travel.PersonalCenter
+{
+    /// <summary>
+    /// PersonalCenter1 的摘要说明
+    /// </summary>
+    public class PersonalCenter1 : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            context.Response.Write("Hello World");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
