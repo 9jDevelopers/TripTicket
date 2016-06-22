a14b0a2b834212496fe2fffa15f6993ea84a9a0a
LLyouenjoy <LLyouenjoy@LLYOUENJOY-PC>
2016-06-22 08:30:25 +0800

    477

---
+++ Travel/Travel/Orderform/Orderform.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Travel.Tool;
+
+namespace Travel.Orderform
+{
+    public partial class Orderform1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ProofTechnique a = new ProofTechnique();
+            a.proofuid(Session["UID"]);
+        }
+    }
+}
